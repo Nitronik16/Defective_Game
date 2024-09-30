0499fcf274bb66bdb870251aba122ac3f57829b4
Koreiscool <740035@student.dusd.net>
2024-09-30 11:49:28 -0700

    stuff

--- Defective/Assets/Scripts/Enemy Scripts/monsterAI.cs
+++ Defective/Assets/Scripts/Enemy Scripts/monsterAI.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class monsterAI : MonoBehaviour
 {
+    [Range(0, 20)] public float speed;
+    private bool isChasing;
+    
+
     // Start is called before the first frame update
     void Start()
     {
